d4f9c3b97887e25503823a2c9c8a92426b8212f6
117503445 <t117503445@gmail.com>
2019-08-14 11:06:14 +0800

    0814-1106

--- static/charts.js
+++ static/charts.js
@@ -1,22 +1,32 @@
+
+
+
 // 基于准备好的dom，初始化echarts实例
 var myChart = echarts.init(document.getElementById('main'));
-window.onresize=function(){
+window.onresize = function () {
     myChart.resize()
 }
-var tdata = []
+searchName("117503445")
 
-$.get("http://127.0.0.1/api/record?name=章赟宸", function (response) {
-    //console.log(response)
-    for (i = 0; i < response.length; i++) {
-        var timeStamp = response[i].TimeStamp
-        var step = response[i].Step
-        var date = new Date(timeStamp * 1000);
-        tdata.push({ value: [date, step] });
-    }
-    setOption()
-});
+function searchName(name) {
+    data = []
+    $.get("http://127.0.0.1/api/record?name=" + name, function (response) {
+        //console.log(response)
+        for (i = 0; i < response.length; i++) {
+            var timeStamp = response[i].TimeStamp
+            var step = response[i].Step
+            var date = new Date(timeStamp * 1000);
+            data.push({ value: [date, step] });
+        }
+        setOption(name)
+    });
+}
 
-function setOption() {
+var data = []
+
+
+
+function setOption(name) {
     var option = {
         tooltip: {
             trigger: 'axis',
@@ -38,33 +48,19 @@ function setOption() {
             }
         },
         series: [{
-            name: 'zyc',
+            name: name,
             type: 'line',
             showSymbol: false,
             hoverAnimation: false,
-            data: tdata
+            data: data
         }]
     };
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option);
 }
 
-
-function randomData() {
-    now = new Date(+now + oneDay);
-    value = value + Math.random() * 21 - 10;
-    return {
-        value: [
-            [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
-            100
-        ]
-    }
-}
-
-var data = [];
-var now = +new Date(2019, 8, 13);
-var oneDay = 24 * 3600 * 1000;
-var value = Math.random() * 1000;
-for (var i = 0; i < 10; i++) {
-    data.push(randomData());
+function search() {
+    var name = $('#selectName option:selected').text()
+    searchName(name)
+    console.log(name)
 }
